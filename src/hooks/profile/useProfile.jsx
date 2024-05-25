086c1a7ab2551143e0a5353deb97bf9299b6bff0
rydlande <rydlande@hotmail.com>
2024-05-25 21:44:00 +0200

    changed how the profile is updated, now the fetch and put is in store and the update should work as expected (i hope)

--- src/hooks/profile/useProfile.jsx
+++ src/hooks/profile/useProfile.jsx
@@ -31,5 +31,36 @@ export const useProfileStore = create((set) => ({
         console.error('Failed to fetch profile data', error);
         set({ error: error.message, loading: false });
       }
-    }
+    },
+    updateProfile: async (updatedProfile) => {
+      set({ loading: true });
+      const accessToken = localStorage.getItem('accessToken');
+      const apiKey = localStorage.getItem('apiKey');
+      const user = JSON.parse(localStorage.getItem('user'));
+
+      if (!accessToken || !apiKey || !user || !user.name) {
+          set({ error: 'Missing access token, API key, or user information', loading: false });
+          return;
+      }
+
+      try {
+          const res = await fetch(`https://v2.api.noroff.dev/holidaze/profiles/${user.name}`, {
+              method: 'PUT',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${accessToken}`,
+                  'X-Noroff-API-Key': `${apiKey}`,
+              },
+              body: JSON.stringify(updatedProfile)
+          });
+          if (!res.ok) {
+              throw new Error(`HTTP error! status: ${res.status}`);
+          }
+          const data = await res.json();
+          set({ profile: data.data, loading: false });
+      } catch (error) {
+          console.error('Failed to update profile data', error);
+          set({ error: error.message, loading: false });
+      }
+  }
   }));
