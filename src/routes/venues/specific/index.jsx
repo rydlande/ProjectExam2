92e949ab4c6bddc6279187d7c493b538a8c74e3a
rydlande <rydlande@hotmail.com>
2024-05-26 12:11:04 +0200

    made a store for specific venue with GET, POST, PUT and DELETE. Updated from fetch to store in the venues/specific/index to get the venue by id

--- src/routes/venues/specific/index.jsx
+++ src/routes/venues/specific/index.jsx
@@ -1,37 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { SpecificVenueCard } from '../../../components/venues/cards/specific';
+import { useSpecificVenueStore } from '../../../hooks/venues/useSpecificVenue'
+
 
 export function SpecificVenue() {
   const { id } = useParams();
-  const [venue, setVenue] = useState({});
-  const [loading, setLoading] = useState(true);
+  const { venue, loading, fetchVenue } = useSpecificVenueStore();
 
   useEffect(() => {
-    async function fetchVenue() {
-      try {
-        const res = await fetch('https://v2.api.noroff.dev/holidaze/venues/' + id + '?_bookings=true&_owner=true');
-        const data = await res.json();
-        setVenue(data.data);
-        console.log(data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Failed to fetch venue', error);
-        setLoading(false);
-      }
-    }
-    fetchVenue();
+    fetchVenue(id);
   }, [id]);
 
   if (loading) {
     return <div>Loading...</div>;
   };
 
-    return (
-      <>
-        <div>
-          <SpecificVenueCard venue={venue} />
-        </div>
-      </>
-    )
-  }
+  return (
+    <>
+      <div>
+        <SpecificVenueCard venue={venue} />
+      </div>
+    </>
+  );
+}
